1ea6a01178de9b6612841a60431c3c04bf6dfdd5
tobi.dara <tobi_daramola@yahoo.com>
2017-07-31 21:52:15 +0100

    find a basis for arraylength and iteration comparison

--- aritGeo.js
+++ aritGeo.js
@@ -1,23 +1,33 @@
 function aritGeo(array) {
+    // initialize variables j and k
+    var j = 0;
+    var k = 0;
 
-    var j, k = 0;
-
+    // use a for loop to test for equality between difference in nth and (n-1)th term
     for (i = 0; i < array.length; i++) {
+
+        // perform test for arithmetic progression and increment j for each equality
         if ((array[i] - array[i + 1]) == (array[i + 1] - array[i + 2])) {
-            j++
+            j++;
+
+            // perform test  for geometrc progression and increment k for each equality
         } else if ((array[i] / array[i + 1]) == (array[i + 1] / array[i + 2])) {
-            k++
-        } else if (array == []) {
-            console.log("0")
-        } else { console.log("-1") }
+            k++;
+        }
 
     }
-    if (j == array.length) {
-        console.log("Arithmetic")
-    } else if (k == array.length) {
-        console.log("Geometric")
+
+    // compare incremental value with array length
+    if (j + 1 == array.length - 1) {
+        return ("Arithmetic");
+    } else if (k + 1 == array.length - 1) {
+        return ("Geometric");
+    } else if (array.length == 0) {
+        return ("0");
+    } else {
+        return ("-1");
     }
 
 }
 
-aritGeo([2, 1, 3, 4, 5, 6])
+aritGeo([4, 6, 8, 10, 12, 14])
