5742c04a2b57d7ac8d274563121d94fc72cd1e63
tobi.dara <tobi_daramola@yahoo.com>
2017-08-01 11:54:53 +0100

    implemented changes on geometric

--- aritGeo.js
+++ aritGeo.js
@@ -1,7 +1,7 @@
 function aritGeo(array) {
     // initialize variables j and k
     var j = 0;
-    var k = 0;
+    var k = 1;
 
     // use a for loop to test for equality between difference in nth and (n-1)th term
     for (i = 0; i < array.length - 1; i++) {
@@ -22,34 +22,21 @@ function aritGeo(array) {
         return ("Arithmetic");
     } else if (k == array.length - 1) {
         return ("Geometric");
-        console.log("Geometric")
-
-        // check for empty array
     } else if (array.length === 0) {
         return ("0");
-
-        //ensure all elements within array are numbers
     } else if (!array.some(isNaN)=== false) {
         return ("number only")
-
-        // ensure array doesn't contain only one item
     } else if (array.length < 2) {
         return ("not a sequence")
-
-        // ensure input element is an array
     } else if (Array.isArray(array) === false) {
         return ("not an array")
-
-        // check for first element cannot be zero for geometric progression
     } else if(array[0] == 0){
       return ("first element cannot be zero in geometric sequence");
     }
-    //else return -1
     else {
         return ("-1");
-        console.log("-1")
    }
 
 }
 
-aritGeo([4,8,16,32,64,128])
+aritGeo([8,16,32,64,128])
